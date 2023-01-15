cdd88fc511e1cbb554b4b2f2f19c4c9a2499c7e0
JulianaVelasques <juliabalta16@gmail.com>
2023-01-15 15:05:55 -0300

    style: chart container component to match layout

--- src/components/ChartContainer/ChartContainer.tsx
+++ src/components/ChartContainer/ChartContainer.tsx
@@ -1,7 +1,25 @@
+import "./ChartContainer.scss";
+
 function ChartContainer() {
   return (
-    <div>
-      <h1>Chart container aqui</h1>
+    <div className="chart-container">
+      <h1>Spending - Last 7 days</h1>
+
+      <div>Chart here</div>
+
+      <hr className="divider" />
+
+      <div className="resume-content">
+        <div>
+          <p className="title">Total this month</p>
+          <p className="money">$478.33</p>
+        </div>
+
+        <div>
+          <p className="percent">+2.4%</p>
+          <p className="title">from last month</p>
+        </div>
+      </div>
     </div>
   );
 }
