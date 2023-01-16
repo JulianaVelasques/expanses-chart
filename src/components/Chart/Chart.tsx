ec18edf39bc8389adb5401b8341fb205ca2af047
JulianaVelasques <juliabalta16@gmail.com>
2023-01-15 23:42:07 -0300

    style: chart.js to look like the designed chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -5,14 +5,48 @@ Chart.register(...registerables);
 import Data from "../../data/data.json";
 
 function ChartComponent() {
+  let biggestAmount = Data.reduce(
+    (previousLargestNumber, currentLargestNumber) => {
+      return currentLargestNumber.amount > previousLargestNumber.amount
+        ? currentLargestNumber
+        : previousLargestNumber;
+    }
+  );
+
   const labels = Data.map((data) => data.day);
   const datasets = [
     {
       label: "",
       data: Data.map((data) => data.amount),
-      backgroundColor: "hsl(10, 79%, 65%)",
-      borderColor: "hsl(10, 79%, 65%)",
+      backgroundColor: (context: any) => {
+        if (biggestAmount.amount == context.raw) {
+          return "hsl(186, 34%, 60%)";
+        } else {
+          return "hsl(10, 79%, 65%)";
+        }
+      },
+      borderColor: (context: any) => {
+        if (biggestAmount.amount == context.raw) {
+          return "hsl(186, 34%, 60%)";
+        } else {
+          return "hsl(10, 79%, 65%)";
+        }
+      },
       borderWidth: 2,
+      hoverBackgroundColor: (context: any) => {
+        if (biggestAmount.amount == context.raw) {
+          return "hsl(186, 34%, 80%)";
+        } else {
+          return "hsl(10, 79%, 80%)";
+        }
+      },
+      hoverBorderColor: (context: any) => {
+        if (biggestAmount.amount == context.raw) {
+          return "hsl(186, 34%, 80%)";
+        } else {
+          return "hsl(10, 79%, 80%)";
+        }
+      },
     },
   ];
 
@@ -23,7 +57,19 @@ function ChartComponent() {
         options={{
           plugins: {
             tooltip: {
-              callbacks: {},
+              callbacks: {
+                title: (context) => {
+                  let title = context[0].formattedValue;
+                  return `$ ${title}`;
+                },
+                label: () => {
+                  return "";
+                },
+              },
+              displayColors: false,
+              yAlign: "bottom",
+              caretPadding: 5,
+              caretSize: 0,
             },
             legend: {
               display: false,
