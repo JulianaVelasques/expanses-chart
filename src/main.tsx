6c3303dc8834c3db4e18dfb655743f02c3c599ac
JulianaVelasques <juliabalta16@gmail.com>
2023-01-16 14:51:10 -0300

    fix: deploy error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./styles/Global.scss";
+import "./Global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
